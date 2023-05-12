3053809aaf829a7d9f4700076fc004458613d0ea
endesew bantayehu <endesew12@gmail.com>
2023-05-12 14:41:45 -0700

    AirBnB clone

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+The module to create file storage in handy way
+"""
+
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+The module that creates Amenity class
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    This class handle the amenity public class attributes
+    """
+    name = ""
