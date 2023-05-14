b87fdfd9774f4444368ca8930383459315f5288d
Adika1630 <addisu05mulat@gmail.com>
2023-05-14 09:47:54 -0700

    AirBnB clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
-"""
-The module to save files in the storage
+# -*- coding: utf-8 -*-
+"""File Storage Module
+This module is in charge of the storage of the
+classes and their management.
 """
 
 import json
@@ -16,29 +17,37 @@ from os import path
 
 
 class FileStorage:
+    """File Storage Class
+    This is the File Storage module.
+    Attributes:
+        __file_path (str): This is the path of the JSON file in which
+            the contents of the `__objects` variable will be stored.
+        __objects (dict): This store all the instances data
     """
-    This class stores files in specified path
-    """
-    __file_path = 'file.json'
+    __file_path = 'objects.json'
     __objects = {}
 
     def all(self):
-        """
-        return: returns all the objects saved
+        """Gets the __objects info
+        Returns the content of the `__objects` class attribute.
         """
         return self.__objects
 
     def new(self, obj):
-        """
+        """Saves a new object in the `__objects` class attribute
         Args:
-            obj: adds new object to objects
+            obj (inst): The object to adds in the `__objects` class attribute
+        Sets in the `__objects` class attribute the instance data
+        with a key as <obj class name>.id.
         """
         key = obj.__class__.__name__ + '.' + obj.id
         self.__objects[key] = obj
 
     def save(self):
-        """
-        This method saves the objects as a json file
+        """Serializes the content of `__objects` class attribute
+        The content of `__objects` class attribute will be serialized
+        to the path of `__file_path` class attribute in JSON format
+        with the `created_at` and `updated_at` formatted.
         """
         json_dict = {}
         for k, v in self.__objects.items():
@@ -47,8 +56,10 @@ class FileStorage:
             f.write(json.dumps(json_dict))
 
     def reload(self):
-        """
-        This method reloads from json format to dictionary format
+        """Deserializes the JSON file in `__file_path` class attribute
+        If the file on `__file_path` class attribute exists, each object
+        on the file will be deserialized and appended to the `__objects`
+        class attribute like an instance with the object data.
         """
         if path.exists(self.__file_path):
             with open(self.__file_path, mode='r', encoding='utf-8') as f:
