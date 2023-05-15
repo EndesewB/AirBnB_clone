b0a8020a94f1ada71fa4ed6d07c51c3f26e6d424
Adika1630 <addisu05mulat@gmail.com>
2023-05-14 22:37:08 -0700

    AiRnB clone

--- tests/test_pep8.py
+++ tests/test_pep8.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Test"""
-from amenity import Amenity
+from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
